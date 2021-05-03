5aec48b00e25e5a99f081a66b97ec9dafd31cd23
bens1320 <yu1piao09@163.com>
2021-05-04 04:32:07 +0800

    Button: add size property style

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,20 +1,35 @@
 <template>
-  <button class="ben-button" :class="`ben-theme-${theme}`">
+  <button class="ben-button" :class="classes">
     <slot/>
   </button>
 </template>
 
 <script lang="ts">
+import {computed} from 'vue'
 export default {
   props: {
     theme: {
       type: String,
       default: 'button'
+    },
+    size:{
+      type: String,
+      default: 'normal'
     }
+  },
+  setup(props){
+    const {theme, size} = props
+    const classes = computed(()=>{
+      return {
+        [`ben-theme-${theme}`]: theme,
+        [`ben-size-${size}`]: size,
+      }
+    })
+    return {classes}
   }
+
 };
 </script>
-
 <style lang="scss">
 $h: 32px;
 $border-color: #d9d9d9;
@@ -35,42 +50,48 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
-
   & + & {
     margin-left: 8px;
   }
-
   &:hover,
   &:focus {
     color: $blue;
     border-color: $blue;
   }
-
   &:focus {
     outline: none;
   }
-
   &::-moz-focus-inner {
     border: 0;
   }
-
   &.ben-theme-link {
     border-color: transparent;
     box-shadow: none;
     color: $blue;
-
-    &:hover, &:focus {
+    &:hover,
+    &:focus {
       color: lighten($blue, 10%);
     }
   }
-
   &.ben-theme-text {
     border-color: transparent;
     box-shadow: none;
     color: inherit;
-
-    &:hover, &:focus {
-      background: darken(white, 5%);;
+    &:hover,
+    &:focus {
+      background: darken(white, 5%);
+    }
+  }
+  &.ben-theme-button{
+    &.ben-size-big{
+      font-size: 24px;
+      height: 48px;
+      padding: 0 16px
+    }
+    &.ben-size-small{
+      font-size: 12px;
+      height: 20px;
+      padding: 0 4px;
     }
   }
 }
