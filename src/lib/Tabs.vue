d23456c733734e4656b75095b8d0a6a8c25d43d4
bens1320 <yu1piao09@163.com>
2021-05-04 23:28:57 +0800

    Tabs: render tabs content

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,22 +1,25 @@
 <template>
   <div>
-    Tabs
-    <component :is="defaults[0]"/>
-    <component :is="defaults[1]"/>
+    <div v-for="(t, index) in titles" :key="index">{{t}}</div>
+    <component v-for="(c, index) in defaults" :key="index" :is="c"/>
   </div>
 </template>
 
 <script lang="ts">
-import Tab from '../lib/Tab.vue'
+import Tab from '../lib/Tab.vue';
+
 export default {
-  setup(props, context){
-    const defaults = context.slots.default()
-    defaults.forEach((tag)=>{
-      if(tag.type !== Tab){
-        throw new Error('Tabs 子组件必须是Tab')
+  setup(props, context) {
+    const defaults = context.slots.default();
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
+        throw new Error('Tabs 子组件必须是Tab');
       }
-    })
-    return {defaults}
+    });
+    const titles = defaults.map((tag) => {
+      return tag.props.title;
+    });
+    return {defaults, titles};
   }
-}
+};
 </script>
