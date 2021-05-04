335cbedc05750582739e2c08c13562dac5d49689
bens1320 <yu1piao09@163.com>
2021-05-05 06:05:09 +0800

    Tabs: optimize code

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -3,10 +3,10 @@
     <div class="ben-tabs-nav" ref="container">
       <div
           class="ben-tabs-nav-item"
-          :class="{selected: t === selected}"
-          v-for="(t,index) in titles" :key="index"
-          :ref="el => {if(el)  navItems[index] = el}"
-          @click="select(t)"
+          v-for="(t,index) in titles"
+          :ref="el => { if (t===selected) selectedItem = el }"
+          @click="select(t)" :class="{selected: t=== selected}"
+          :key="index"
       >
         {{t}}
       </div>
@@ -24,7 +24,7 @@
 
 <script lang="ts">
 import Tab from '../lib/Tab.vue';
-import {ref, onMounted, onUpdated} from 'vue';
+import {ref, onMounted, onUpdated, watchEffect} from 'vue';
 
 export default {
   props: {
@@ -33,20 +33,20 @@ export default {
     }
   },
   setup(props, context) {
-    const navItems = ref<HTMLDivElement[]>([]);
+    const selectedItem = ref<HTMLDivElement>(null);
     const indicator = ref<HTMLDivElement>(null);
     const container = ref<HTMLDivElement>(null);
     const x = () => {
-      const divs = navItems.value;
-      const result = divs.filter(div => div.classList.contains('selected'))[0];
-      const {width} = result.getBoundingClientRect();
+      const {
+        width
+      } = selectedItem.value.getBoundingClientRect();
       indicator.value.style.width = width + 'px';
       const {
         left: left1
       } = container.value.getBoundingClientRect();
       const {
         left: left2
-      } = result.getBoundingClientRect();
+      } = selectedItem.value.getBoundingClientRect();
       const left = left2 - left1;
       indicator.value.style.left = left + 'px';
     };
@@ -71,7 +71,7 @@ export default {
       defaults,
       titles,
       select,
-      navItems,
+      selectedItem,
       indicator,
       container
     };
