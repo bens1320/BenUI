44e98b329cc6aa9f6024f0b4c1d3d82719ea65d9
bens1320 <yu1piao09@163.com>
2021-05-04 17:04:18 +0800

    Dialog: support Teleport to Body

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,21 +1,23 @@
 <template>
   <template v-if="visible">
-    <div class="ben-dialog-overlay" @click="onClickOverlay"></div>
-    <div class="ben-dialog-wrapper">
-      <div class="ben-dialog">
-        <header>
-          <slot name="title"/>
-          <span class="ben-dialog-close" @click="close"></span>
-        </header>
-        <main>
-          <slot name="content"/>
-        </main>
-        <footer>
-          <Button level="main" @click="ok">OK</Button>
-          <Button @click="cancel">Cancel</Button>
-        </footer>
+    <Teleport to="body">
+      <div class="ben-dialog-overlay" @click="onClickOverlay"></div>
+      <div class="ben-dialog-wrapper">
+        <div class="ben-dialog">
+          <header>
+            <slot name="title"/>
+            <span class="ben-dialog-close" @click="close"></span>
+          </header>
+          <main>
+            <slot name="content"/>
+          </main>
+          <footer>
+            <Button level="main" @click="ok">OK</Button>
+            <Button @click="cancel">Cancel</Button>
+          </footer>
+        </div>
       </div>
-    </div>
+    </Teleport>
   </template>
 </template>
 
