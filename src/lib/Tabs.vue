c725077adbadbef9ff6ac2f00c718c005ec029d6
bens1320 <yu1piao09@163.com>
2021-05-04 23:23:46 +0800

    Tabs: judge child components type

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
     Tabs
+    <component :is="defaults[0]"/>
+    <component :is="defaults[1]"/>
   </div>
 </template>
+
+<script lang="ts">
+import Tab from '../lib/Tab.vue'
+export default {
+  setup(props, context){
+    const defaults = context.slots.default()
+    defaults.forEach((tag)=>{
+      if(tag.type !== Tab){
+        throw new Error('Tabs 子组件必须是Tab')
+      }
+    })
+    return {defaults}
+  }
+}
+</script>
