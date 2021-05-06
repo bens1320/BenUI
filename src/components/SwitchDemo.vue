48e16e4955980d84c247ec1cee10303bbbb5e28b
bens1320 <yu1piao09@163.com>
2021-05-06 10:41:13 +0800

    display source code

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -2,32 +2,32 @@
   <div>
     <h1>Switch 组件示例 </h1>
     <div class="demo">
-      <div class="demo">
-        <h2>常规用法</h2>
-        <div class="demo-component">
-          <Switch1Demo/>
-        </div>
-        <div class="demo-actions">
-          <Button>查看代码</Button>
-        </div>
-        <div class="demo-code">
-          <pre>&lt;Switch v-model:value="bool" disabled /&gt;</pre>
-        </div>
+      <h2>常规用法</h2>
+      <div class="demo-component">
+        <component :is="Switch1Demo" />
       </div>
-      <div class="demo">
-        <h2>支持 disabled </h2>
-        <div class="demo-component">
-          <Switch2Demo/>
-        </div>
-        <div class="demo-actions">
-          <Button>查看代码</Button>
-        </div>
-        <div class="demo-code">
-          <pre>&lt;Switch v-model:value="bool" disabled /&gt;</pre>
-        </div>
+      <div class="demo-actions">
+        <Button>查看代码</Button>
+      </div>
+      <div class="demo-code">
+        <pre>{{Switch1Demo.__sourceCode}}</pre>
       </div>
     </div>
+    <div class="demo">
+      <h2>支持 disabled </h2>
+      <div class="demo-component">
+        <component :is="Switch2Demo" />
+      </div>
+      <div class="demo-actions">
+        <Button>查看代码</Button>
+      </div>
+      <div class="demo-code">
+        <pre>{{Switch2Demo.__sourceCode}}</pre>
+      </div>
+    </div>
+  </div>
 </template>
+
 <script lang="ts">
 import Switch from '../lib/Switch.vue';
 import Button from '../lib/Button.vue';
@@ -39,15 +39,14 @@ import {
 
 export default {
   components: {
-    Switch1Demo,
-    Switch2Demo,
-    Switch,
     Button
   },
   setup() {
     const bool = ref(false);
     return {
-      bool
+      bool,
+      Switch1Demo,
+      Switch2Demo
     };
   }
 };
